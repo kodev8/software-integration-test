2ad2ab3323d991e563f5c17766aa6ef35e3ecb06
Kalev Keil <103848034+kodev8@users.noreply.github.com>
2024-08-02 01:03:06 +0200

    chore: Add Jest configuration for TypeScript project

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/tests/**/*.test.ts'],
+    transform: {
+        '^.+\\.tsx?$': ['ts-jest', {}],
+    },
+    testTimeout: 70000,
+    verbose: true,
+    testPathIgnorePatterns: ['/node_modules/', '/dist/', '/coverage/'],
+    coveragePathIgnorePatterns: [
+        '/node_modules/',
+        '/dist/',
+        '.mockData.ts',
+        '/boot/',
+        '/config/',
+    ],
+    roots: ['<rootDir>/src'],
+};
+
+export default config;
