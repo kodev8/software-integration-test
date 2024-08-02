868ece9f2a104f84d01deb7fca83fe3c3cb3b1d0
Alago Victor <123035877+victor-alago@users.noreply.github.com>
2024-08-02 21:34:18 +0200

    feat: Add logout functionality to profile controller and routes

--- src/routes/profile.routes.ts
+++ src/routes/profile.routes.ts
@@ -4,5 +4,6 @@ import * as profileServices from '../controllers/profile.controller';
 const router: Router = Router();
 
 router.put('/', profileServices.editPassword);
+router.post('/', profileServices.logout);
 
 export default router;
