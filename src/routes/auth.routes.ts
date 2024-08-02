03f9a08a15d0d7ba6c6eb08e3174f7001e1f888f
Grigoli2001 <gpatsatsiausa@gmail.com>
2024-08-02 05:24:10 +0200

    feat: Add login route and controller for user signin

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,5 +4,6 @@ import authServices from '../controllers/auth.controller';
 const router = express.Router();
 
 router.post('/signup', authServices.signup);
+router.post('/login', authServices.signin);
 
 export default router;
