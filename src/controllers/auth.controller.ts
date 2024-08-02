a5d7e32d6cdfc1e25679abd9ae45866684685a33
Grigoli2001 <gpatsatsiausa@gmail.com>
2024-08-02 05:24:10 +0200

    feat: Add login route and controller for user signin

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,11 @@
 import { Response } from 'express';
-import userModel from '../models/userModel';
+import userModel, { IUserDocument } from '../models/userModel';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 // import { Session, SessionData } from 'express-session';
 import { AuthRequest } from '../types/customRequests.interface';
 import statusCodes from '../constants/statusCodes';
+import logger from '../middleware/winston';
 
 const signup = async (req: AuthRequest, res: Response): Promise<Response> => {
     const { username, email, password } = req.body;
@@ -31,6 +33,52 @@ const signup = async (req: AuthRequest, res: Response): Promise<Response> => {
     }
 };
 
+const signin = async (req: AuthRequest, res: Response): Promise<Response> => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res
+            .status(statusCodes.badRequest)
+            .json({ error: 'missing information' });
+    }
+
+    try {
+        const user: IUserDocument | null = await userModel.findOne({ email });
+
+        if (!user) {
+            return res
+                .status(statusCodes.badRequest)
+                .json({ message: 'User not found' });
+        }
+
+        if (!bcrypt.compareSync(password, user.password)) {
+            return res
+                .status(statusCodes.badRequest)
+                .json({ message: "Email or password don't match" });
+        }
+
+        req.session.user = {
+            _id: user._id.toString(),
+        };
+
+        const token = jwt.sign(
+            { user: { id: user._id, email: user.email } },
+            process.env.JWT_SECRET_KEY as string,
+            {
+                expiresIn: '1h',
+            }
+        );
+
+        return res.status(statusCodes.success).json({ token });
+    } catch (error) {
+        logger.error('Error while getting user from DB', error.message);
+        return res
+            .status(statusCodes.queryError)
+            .json({ error: 'Failed to get user' });
+    }
+};
+
 export default {
     signup,
+    signin,
 };
