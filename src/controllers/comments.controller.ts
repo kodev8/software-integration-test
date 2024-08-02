bc213bdd4dfe44dc01dc98d09905ff947bc3aaa4
Caleb ESHO <110430708+the-eshocaleb@users.noreply.github.com>
2024-08-02 22:47:11 +0200

    feat: Add getCommentsById functionality and route

--- src/controllers/comments.controller.ts
+++ src/controllers/comments.controller.ts
@@ -48,6 +48,32 @@ export const addComment = async (
     }
 };
 
+export const getCommentsById = async (
+    req: CommentRequest,
+    res: Response
+): Promise<Response> => {
+    const { movie_id } = req.params;
+
+    const movieId = parseInt(movie_id);
+
+    if (!movie_id || isNaN(movieId)) {
+        return res
+            .status(statusCodes.badRequest)
+            .json({ message: 'movie id missing' });
+    }
+
+    try {
+        const comments = await commentModel.find({ movie_id: movieId });
+        return res.status(statusCodes.success).json({ comments });
+    } catch (error) {
+        logger.error(error.stack);
+        return res
+            .status(statusCodes.queryError)
+            .json({ error: 'Exception occurred while fetching comments' });
+    }
+};
+
 export default {
+    getCommentsById,
     addComment,
 };
